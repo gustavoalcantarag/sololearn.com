33890c4467d7e742f9c84ac584c0c5a5cab6aae2
Gustavo Alcántara Guerrero <tavo.alcantara@gmail.com>
2022-02-15 23:19:46 -0500

    commit

--- sololearn_3.1_lesson/Program.cs
+++ sololearn_3.1_lesson/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+            Console.WriteLine("");
+            //
+            int x = 89;
+            Console.WriteLine(x);
+            //
+            int z = 10;
+            double y = 20;
+            int xx = 29;
+            Console.WriteLine("x = {0}; y = {1}; xx = {2}", z, y, x);
+        }
+    }
+}
