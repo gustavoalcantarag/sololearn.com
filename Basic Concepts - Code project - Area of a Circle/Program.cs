791c84d19e751dd34d1fb2272a623b433f58edb7
Gustavo Alcántara Guerrero <tavo.alcantara@gmail.com>
2022-02-24 00:41:51 -0500

    commit

---
+++ Basic Concepts - Code project - Area of a Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius;
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(pi * (radius * radius));
+        }
+    }
+}
