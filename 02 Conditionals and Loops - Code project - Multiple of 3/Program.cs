d5d1e14d4728f16064919553d43ceb31d8688a52
Gustavo Alcántara Guerrero <tavo.alcantara@gmail.com>
2022-02-27 18:49:55 -0500

    commit

---
+++ 02 Conditionals and Loops - Code project - Multiple of 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            string value = "";
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    value = value + "*";
+                    continue;
+                }
+                value = value + i.ToString();
+            }
+            Console.WriteLine(value);
+        }
+    }
+}
