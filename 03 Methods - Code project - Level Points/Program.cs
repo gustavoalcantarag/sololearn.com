a86a7b45be52ed97784b81a5854e2910273c3e51
Gustavo Alcántara Guerrero <tavo.alcantara@gmail.com>
2022-02-28 23:44:02 -0500

    commit

---
+++ 03 Methods - Code project - Level Points/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Points(levels));
+        }
+
+        static int Points(int levels)
+        {
+            if (levels == 1)
+            {
+                return 1;
+            }
+            return levels + Points(levels - 1);
+        }
+    }
+}
