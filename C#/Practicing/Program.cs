1e97b8458ce1ed7699898f5ae25af59962f78a0e
Gustavo Alcántara Guerrero <tavo.alcantara@gmail.com>
2022-03-04 23:02:03 -0500

    commit

--- C#/Practicing/Program.cs
+++ C#/Practicing/Program.cs
@@ -226,46 +226,46 @@
 //    }
 //}
 
-//using System;
-//using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-//namespace Code_Coach_Challenge
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            string postText = Console.ReadLine();
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string postText = Console.ReadLine();
 
-//            Post post = new Post();
-//            post.Text = postText;
-//            post.ShowPost();
+            Post post = new Post();
+            post.Text = postText;
+            post.ShowPost();
 
-//        }
-//    }
+        }
+    }
 
-//    class Post
-//    {
-//        private string text;
+    class Post
+    {
+        private string text;
 
-//        //write a constructor here
-//        public Post()
-//        {
-//            text = Text;
-//        }
+        //write a constructor here
+        public Post()
+        {
+            text = Text;
+        }
 
-//        public void ShowPost()
-//        {
-//            Console.WriteLine("New post");
-//            Console.WriteLine(text);
-//        }
+        public void ShowPost()
+        {
+            Console.WriteLine("New post");
+            Console.WriteLine(text);
+        }
 
-//        //write a property for member text
-//        public string Text
-//        {
-//            get { return text; }
-//            set { text = value; }
-//        }
+        //write a property for member text
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
 
-//    }
-//}
+    }
+}
